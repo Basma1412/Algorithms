24339706b8e723d789a0947c229ed93836b85cd8
Basma1412 <13p6036@eng.asu.edu.eg>
2016-12-29 21:56:51 +0200

    definitions

---
+++ src/algorithm/project/AlgorithmProject.java
@@ -0,0 +1,41 @@
+
+package algorithm.project;
+
+class location
+{
+    int x;
+    int y;
+    public location(int x , int y )
+    {
+        this.x=x;
+        this.y=y;
+    }
+    public int getX()
+    {
+        return this.x;
+    }
+    public int getY()
+    {
+        return this.y;
+    }
+    public void  setX(int x)
+    {
+        this.x=x;
+    }
+    public void setY(int y)
+    {
+        this.y=y;
+    }
+}
+
+class Nodes{
+    
+}
+
+
+public class AlgorithmProject {
+
+    public static void main(String[] args) {
+    }
+    
+}
