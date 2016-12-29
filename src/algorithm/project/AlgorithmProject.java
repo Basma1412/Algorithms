689bac96d26beec91443a9a9d1b4b4627a6dcba3
Basma1412 <13p6036@eng.asu.edu.eg>
2016-12-29 21:58:08 +0200

    definitions

--- src/algorithm/project/AlgorithmProject.java
+++ src/algorithm/project/AlgorithmProject.java
@@ -1,35 +1,91 @@
 
 package algorithm.project;
 
-class location
+class Location
 {
-    int x;
-    int y;
-    public location(int x , int y )
+    double x;
+    double y;
+    public Location(double x , double y )
     {
         this.x=x;
         this.y=y;
     }
-    public int getX()
+    public double getX()
     {
         return this.x;
     }
-    public int getY()
+    public double getY()
     {
         return this.y;
     }
-    public void  setX(int x)
+    public void  setX(double x)
     {
         this.x=x;
     }
-    public void setY(int y)
+    public void setY(double y)
     {
         this.y=y;
     }
 }
 
-class Nodes{
+class Message{
     
+}
+
+class Node{
+    int id;
+    Location location;
+    int batteryPower;
+    
+    public Node(int id,Location location,int batteryPower)
+    {
+        this.id=id;
+        this.location=location;
+        this.batteryPower=batteryPower;
+    }
+     
+    public Location getLocation()
+    {
+        return this.location;
+    }
+    public void  setLocation(Location loc)
+    {
+        this.location=loc;
+    }
+    
+   public boolean inRange(Node mobile)
+   {
+       double locX = this.location.getX();
+       double locY = this.location.getY();
+       
+       
+       double locX2 = mobile.location.getX();
+       double locY2 = mobile.location.getY();
+       
+       double distance ; 
+       
+       double param1=locX2-locX;
+       double param2=locY2-locY;
+       
+       distance=Math.sqrt((Math.pow(param1, 2))+(Math.pow(param2, 2)));
+       
+       return distance<=20;
+       
+   }
+   
+    public void broadcast(Message msg)
+   {
+   }
+   
+   public void send(Message msg)
+   {
+       
+   }
+   
+   public void receive(Message msg)
+   {
+       
+   }
 }
 
 
