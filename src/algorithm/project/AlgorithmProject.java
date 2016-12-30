6cd504a888f5e92af6570d95c360a651f718a248
Basma1412 <13p6036@eng.asu.edu.eg>
2016-12-30 21:15:25 +0200

    more definitions

--- src/algorithm/project/AlgorithmProject.java
+++ src/algorithm/project/AlgorithmProject.java
@@ -1,126 +1,120 @@
-
 package algorithm.project;
 
-class Location
-{
+import java.util.List;
+
+class Location {
+
     double x;
     double y;
-    public Location(double x , double y )
-    {
-        this.x=x;
-        this.y=y;
+
+    public Location(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
-    public double getX()
-    {
+
+    public double getX() {
         return this.x;
     }
-    public double getY()
-    {
+
+    public double getY() {
         return this.y;
     }
-    public void  setX(double x)
-    {
-        this.x=x;
+
+    public void setX(double x) {
+        this.x = x;
     }
-    public void setY(double y)
-    {
-        this.y=y;
+
+    public void setY(double y) {
+        this.y = y;
     }
 }
 
-class Message{
-    
+class Message {
+
     int sender_id;
     int receiver_id;
-    
-    public Message(int sender_id,int receiver_id)
-    {
-        this.sender_id=sender_id;
-        this.receiver_id=receiver_id;
+
+    public Message(int sender_id, int receiver_id) {
+        this.sender_id = sender_id;
+        this.receiver_id = receiver_id;
     }
 }
 
-class Node{
+class Node {
+
     int id;
     Location location;
     double batteryPower;
     double antennaPower;
-    
-    public Node(int id,Location location,double batteryPower,double antennaPower)
-    {
-        this.id=id;
-        this.location=location;
-        this.batteryPower=batteryPower;
-        this.antennaPower=antennaPower;
+    List<Node> neighbors;
+
+    public Node(int id, Location location, double batteryPower, double antennaPower) {
+        this.id = id;
+        this.location = location;
+        this.batteryPower = batteryPower;
+        this.antennaPower = antennaPower;
     }
-     
-    public Location getLocation()
-    {
+
+    public Location getLocation() {
         return this.location;
     }
-    public void  setLocation(Location loc)
-    {
-        this.location=loc;
-    }
-    
-   public boolean inRange(Node mobile)
-   {
-       double locX = this.location.getX();
-       double locY = this.location.getY();
-       
-       
-       double locX2 = mobile.location.getX();
-       double locY2 = mobile.location.getY();
-       
-       double distance ; 
-       
-       double param1=locX2-locX;
-       double param2=locY2-locY;
-       
-       distance=Math.sqrt((Math.pow(param1, 2))+(Math.pow(param2, 2)));
-       
-       return distance<=20;
-       
-   }
-   
-    public void broadcast(Message msg)
-   {
-       if (this.batteryPower>0)
-       {
-         this.batteryPower--;  
-       }
-       else 
-       {
-           
-       }
-   }
-    
-      public void forward(Message msg)
-   {
-   }
-   
-   public void send(Message msg)
-   {
-       
-   }
-   
-   public void receive(Message msg)
-   {
-       if (this.id==msg.receiver_id)
-       {
-           
-       }
-       else 
-       {
-           forward(msg);
-       }
-   }
-}
 
+    public void setLocation(Location loc) {
+        this.location = loc;
+    }
+
+    public boolean inRange(Node mobile) {
+        double locX = this.location.getX();
+        double locY = this.location.getY();
+
+        double locX2 = mobile.location.getX();
+        double locY2 = mobile.location.getY();
+
+        double distance;
+
+        double param1 = locX2 - locX;
+        double param2 = locY2 - locY;
+
+        distance = Math.sqrt((Math.pow(param1, 2)) + (Math.pow(param2, 2)));
+
+        return distance <= 20;
+
+    }
+
+    public void broadcast(Message msg) {
+        if (this.batteryPower > 0) {
+            this.batteryPower--;
+
+            for (Node neighbor : neighbors) {
+                if (!(neighbor.id == msg.sender_id)) {
+                    forward(msg);
+                }
+            }
+        }
+        else {
+            
+        }
+    }
+
+    public void forward(Message msg) {
+    }
+
+    public void send(Message msg) {
+
+    }
+
+    public void receive(Message msg) {
+        if (this.id == msg.receiver_id) {
+
+        } else {
+            forward(msg);
+        }
+    }
+}
 
 public class AlgorithmProject {
 
     public static void main(String[] args) {
     }
-    
+
 }
